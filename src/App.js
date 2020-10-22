dd8e1f43372875add0a23ce7a2a00f2d223f9f3d
Nishant Tomar <nishanttomar@pop-os.localdomain>
2020-10-22 13:03:37 +0530

    checking

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
+import Home from './Checkers/Home';
+import Login from './Checkers/login';
+import Register from './Checkers/register';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Link to="/">Home</Link><br/>
+        <Link to="/login">Login</Link><br/>
+        <Link to="/register">Register</Link>
+
+        <Route exact path="/" component={Home}/>
+        <Route path="/login" component={Login}/>
+        <Route path="/register" component={Register}/>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Checkers/login.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import style from './login.css'
+
+class login extends Component {
+    render() {
+        return (
+            <div>
+                <section id={style.section}>
+                    <h2>Login Working</h2>
+                    <p>Sure</p>
+                </section>
+            </div>
+        );
+    }
+}
+
+export default login;
